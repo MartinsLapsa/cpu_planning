d7b1251fbe34dbc7b2223880abc51afde5701757
Mārtiņš Lapsa <martins.lapsa@makit.lv>
2017-04-01 15:52:01 +0300

    srtf-done

--- js/srtf.js
+++ js/srtf.js
@@ -1,110 +1,32 @@
-var srtf = {
-  step: 0,
-  steps: 0,
-  processes: [],
-  data: [],
-  lastKey: null,
-  $timeline: $('#timeline'),
-
-  perform() {
-    this.reset();
-    for (var i=0; i < this.steps; i++) {
-      this.performStep();
-    }
-    this.showTimeline();
-  },
-
-  reset() {
-    this.data = [];
-    this.step = 0;
-    this.steps = 0;
-    this.lastKey = null;
-    this.resetProcesses();
-  },
-
-  resetProcesses() {
-    steps = 0;
-    this.processes = $('.process').map(function(k){
-      var $this = $(this);
-      var burst = parseInt($this.find('.burst')[0].value);
-      steps += burst;
-
-      return {
-        arrival: parseInt($this.find('.arrival')[0].value),
-        burst: burst,
-        key: k
-      }
-    });
-
-    this.steps = steps;
-  },
-
-  performStep() {
-    var key = this.getProcessKey();
-
-    if ( key != null ) {
-      if ( key === this.lastKey ) {
-        this.data[this.data.length-1].end += 1;
-      }
-      else {
-        var p = {
-          start: this.step,
-          end: this.step + 1,
-          className: 'style--p' + (key+1),
-          id: 'P' + (key+1)
-        }
-        this.data.push(p);
-      }
-      this.processes[key].burst -= 1;
-    }
-    this.lastKey = key;
-    this.step += 1;
-  },
-
+class Srtf extends Visualizer{
   getProcessKey() {
     var key = null;
     var step = this.step;
     var justArrived = _.filter(this.processes, function(p) {
       return p.arrival === step;
     });
+    var previousProcess = this.previousProcess();
 
-    if ( justArrived.length ) {
+    if ( !previousProcess || previousProcess.burst === 0 ) {
+      var waiting = _.filter(this.processes, function(p) { return p.arrival <= step && p.burst > 0 });
+      var shortest =  _.orderBy(waiting, 'burst')[0];
+      key = shortest ? shortest.key : null;
+    }
+    else if ( justArrived.length ) {
       var shortest = _.orderBy(justArrived, 'burst')[0];
-      if ( this.lastKey === null || shortest.burst < this.process().burst ) {
+      if ( this.lastKey === null || shortest.burst < previousProcess.burst ) {
         key = shortest.key;
       }
-    }
-    else if ( this.process().burst === 0 ) {
-      nextProcess = _.filter(this.processes, function(p) { return p.arrival < step && p.burst > 0 })[0];
-      key = nextProcess ? nextProcess.key : null;
+      else{
+        key = this.lastKey;
+      }
     }
     else {
       key = this.lastKey;
     }
 
     return key;
-  },
-
-  process() {
-    return this.processes[this.lastKey];
-  },
-
-  showTimeline() {
-    var options = {
-      phases: [
-        { start: 0, end: this.steps, indicatorsEvery: 1, share: 1 },
-      ],
-      barHeight: 45,
-      fontSize: 16
-    }
-
-    console.table(this.data);
-
-    this.$timeline.show();
-    this.$timeline.simpleTimeline(options, [this.data]);
   }
 }
 
-$('button').on('click', function(){
-  srtf.perform();
-});
+var vis = new Srtf();
